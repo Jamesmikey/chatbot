b3ce2a8a30441b93a8aed29133a9eb92f56bfa15
jamesmikely <jamesmichaely@gmail.com>
2023-11-08 18:13:11 +0300

    Security

--- src/main/java/tz/ac/udsm/chatBot/configs/SecurityConfigs.java
+++ src/main/java/tz/ac/udsm/chatBot/configs/SecurityConfigs.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
@@ -12,14 +14,17 @@ import org.springframework.security.crypto.password.PasswordEncoder;
  * @created 08-11-2023 17:19:38
  */
 @Configuration
+//@EnableWebSecurity
 public class SecurityConfigs extends SecurityConfigurerAdapter {
 
-    @Autowired
-    PasswordEncoder encoder;
 
     @Bean
     public PasswordEncoder createEncoder(){
-        PasswordEncoder bCryptPasswordEncoder= new BCryptPasswordEncoder();
+//        PasswordEncoder bCryptPasswordEncoder= new BCryptPasswordEncoder();
+        PasswordEncoder bCryptPasswordEncoder=  PasswordEncoderFactories.createDelegatingPasswordEncoder();
+
+        //Change Default password encoder;
+
         return bCryptPasswordEncoder;
     }
 }
--- src/main/java/tz/ac/udsm/chatBot/controller/WebController.java
+++ src/main/java/tz/ac/udsm/chatBot/controller/WebController.java
@@ -1,8 +1,8 @@
 package tz.ac.udsm.chatBot.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -23,8 +23,23 @@ public class WebController {
     @Autowired
     private UserService userService;
 
+
+    private final PasswordEncoder passwordEncoder;
+
+    public WebController(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+
+        //        Encoding plaintext to hashed text
+        String encodedText=passwordEncoder.encode("james12@12");
+
+        System.out.println("Encoded Password (james12) :"+encodedText);
+    }
+
     @GetMapping
     public ModelAndView index(ModelAndView modelAndView){
+
+
+
 
         List<User> users=userService.getAll();
 
@@ -32,6 +47,8 @@ public class WebController {
         modelAndView.addObject("name","Home Page");
 
         modelAndView.addObject("users",users);
+
+//        modelAndView.addObject("encodedPassword",encodedText);
 
         modelAndView.setViewName("pages/index");
 
